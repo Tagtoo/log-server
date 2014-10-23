54f7f2cd90e0c62e3d04c2652ec8175f46b11a81
george <goblin_george@gmail.com>
2014-10-23 08:57:57 +0000

    fix script

--- process.sh
+++ process.sh
@@ -13,6 +13,28 @@ do
     i_file=$file
     o_file=`echo ${file}.gz|sed 's/request.log/request.json/'`
     echo "process $i_file"
-    python rtb_upload.py $i_file $o_file && echo "start upload $o_file to gs://$bucket/$o_file" && gsutil cp $o_file gs://$bucket/ && echo "upload end... remove $i_file $o_file" && echo "processed $i_file"&
+    python rtb_upload.py $i_file $o_file 
+
+    if [ $? -ne 0 ]
+    then
+        echo 'process error'
+        break
+    fi
+
+    echo "processed $i_file"&
+
+
+    echo "start upload $o_file to gs://$bucket/$o_file" 
+    gsutil cp $o_file gs://$bucket/ 
+
+    if [ $? -ne 0 ]
+    then
+        echo "upload error"
+        break
+    fi
+    echo "upload success"
+
+    echo "remove  $i_file $o_file"
+    rm $i_file $o_file
 done
 
